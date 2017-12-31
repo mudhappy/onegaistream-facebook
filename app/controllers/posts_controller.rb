0cc7354b0eeb52b0c7a8c9108b9ca479dcd55bcc
Adrian Pad <mudhappy12@gmail.com>
2017-12-30 21:22:13 -0500

    Modelo post, controlador #create #show, ruta /v/:id para show

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+	def index
+		@post = Post.new
+	end
+
+	def create 
+		@post = Post.new(strong_params)
+		if @post.save
+			redirect_to @post
+		else
+			redirect_to posts_path
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+
+
+	private 
+
+	def strong_params
+		params.require(:post).permit(:title, :url)
+	end
+end
