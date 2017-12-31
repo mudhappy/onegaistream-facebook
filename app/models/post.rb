6618f18962c9c2d287c271d62f26eb87b92369e3
Adrian Pad <mudhappy12@gmail.com>
2017-12-30 22:01:57 -0500

    Gema Friendly_id para que el slug(ruta /v/:id) sea con el title /v/mira-esto

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+	validates :title, presence: true, uniqueness: true
+	validates :slug, presence: true, uniqueness: true
+	
+	validates :url, presence: true
+
+	extend FriendlyId
+	friendly_id :title, use: :slugged
 end
